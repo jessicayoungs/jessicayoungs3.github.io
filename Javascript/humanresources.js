05807f6ca74bd3cbfa129cfa697f855892bd6a8a
jessicayoungs <102703167+jessicayoungs@users.noreply.github.com>
2022-10-31 23:49:39 -0600

    resources

---
+++ Javascript/humanresources.js
@@ -0,0 +1,42 @@
+//Problem 1
+{
+    "firstName" : "Sam",
+    "department" : "Tech",
+    "designation" : "Manager",
+    "salary" : "40,000",
+    "raiseEligible" : "true"
+}
+{
+    "firstName" : "Mary",
+    "department" : "Finance",
+    "designation" : "Trainee",
+    "salary" : "18,500",
+    "raiseEligible" : "true"
+}
+{
+    "firstName" : "Bill",
+    "department" : "HR",
+    "designation" : "Executive",
+    "salary" : "21,200",
+    "raiseEligible" : "false"
+}
+
+//Problem 2
+{
+    "companyName" : "Tech Stars",
+    "website" : "www.techstars.site",
+    "employees" : [
+"Sam",
+"Mary",
+"Bill"
+    ]
+}
+
+//Problem 3
+
+
+//Problem 4
+
+//Problem 5
+
+//Problem 6
